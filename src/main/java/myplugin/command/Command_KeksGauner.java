371c9ff9a870eecec3c09347d678398f2b4c1fb3
keksgauner <darklupo5.2003@gmail.com>
2021-07-10 21:52:28 +0200

    Commit

--- src/main/java/myplugin/command/Command_KeksGauner.java
+++ src/main/java/myplugin/command/Command_KeksGauner.java
@@ -24,7 +24,7 @@ public class Command_KeksGauner extends Command {
         if(Check.isPlayer(sender)) return;
         ProxiedPlayer p = (ProxiedPlayer) sender;
         // Check player permission
-        //if(Check.hasPermission(p, "keksgauner.use", "§4Du bist nicht cool genug")) return;
+        if(Check.hasPermission(p, "keksgauner.use", "§4Du bist nicht cool genug")) return;
         p.sendMessage(TextComponentBuilder.create(Data.getConfig().getConfig().getString("example.keksgauner_example")));
         // Example of TextCpmponent
         p.sendMessage(TextComponentBuilder.create(">> ", new TextComponentBuilder("Bin ich COOL?").addHover(" >> Coolnismode").addClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "Nein bist du nicht!").build(), " <<"));
